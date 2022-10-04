3f3a7be90229c810a43a93d8ee928113a02f8301
nsuzuki7713 <nsuzuki7713@gmail.com>
2022-10-04 22:36:46 +0900

    更新

---
+++ packages/nest-sample/stories/StyledButton.stories.tsx
@@ -0,0 +1,29 @@
+import { ComponentMeta } from '@storybook/react';
+import { StyledButton } from '../components/StyledButton'
+
+// ファイル内のStoryの設定(メタデータオブジェクト)
+export default {
+  // グループ名
+  title: 'StyledButton',
+  // 使用するコンポーネント
+  component: StyledButton,
+  argTypes: { onClick: { action: 'clicked' } },
+} as ComponentMeta<typeof StyledButton>;
+
+export const Primary = (props) => {
+  return (
+    <StyledButton {...props} variant="primary">Primary</StyledButton>
+  )
+}
+
+export const Success = (props) => {
+  return (
+    <StyledButton {...props} variant="success">Primary</StyledButton>
+  )
+}
+
+export const Transparent = (props) => {
+  return (
+    <StyledButton {...props} variant="transparent">Primary</StyledButton>
+  )
+}
