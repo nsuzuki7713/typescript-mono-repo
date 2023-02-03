e3a4ac6567fa30f01dde75ab4733b65e677c8fc4
nsuzuki7713 <nsuzuki7713@gmail.com>
2023-02-04 01:25:31 +0900

    署名を検証する

--- packages/line-bot/src/app.module.ts
+++ packages/line-bot/src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LineWebhookController } from './line-webhook/line-webhook.controller';
 import { LineWebhookModule } from './line-webhook/line-webhook.module';
 
 @Module({
-  imports: [LineWebhookModule],
+  imports: [LineWebhookModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController, LineWebhookController],
   providers: [AppService],
 })
--- packages/line-bot/src/line-webhook/line-webhook.controller.ts
+++ packages/line-bot/src/line-webhook/line-webhook.controller.ts
@@ -1,11 +1,24 @@
 import { Body, Controller, Headers, Post } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as crypto from 'crypto';
 
 @Controller('line-webhook')
 export class LineWebhookController {
+  constructor(private configService: ConfigService) {}
+
   @Post()
   webhook(@Body() body: Record<string, unknown>, @Headers() headers): string {
-    // todo 署名を検証する
+    // 署名を検証する
     // https://developers.line.biz/ja/docs/messaging-api/receiving-messages/#verifying-signatures
+    const channelSecret = this.configService.get<string>('CHANNEL_SECRET');
+    console.log(channelSecret);
+    const signature = crypto
+      .createHmac('sha256', channelSecret)
+      .update(JSON.stringify(body))
+      .digest('base64');
+    if (signature !== headers['x-line-signature']) {
+      throw new Error('署名が正しくありません。');
+    }
 
     console.log('header');
     console.log(JSON.stringify(headers));
