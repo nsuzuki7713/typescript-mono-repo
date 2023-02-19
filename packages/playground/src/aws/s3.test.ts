ac122b1374d8897991e39766c1a2a631f41d6378
nsuzuki7713 <nsuzuki7713@gmail.com>
2023-02-19 23:45:15 +0900

    追加

--- packages/playground/src/aws/s3.test.ts
+++ packages/playground/src/aws/s3.test.ts
@@ -5,11 +5,11 @@ dotenv.config();
 
 describe('S3Client', () => {
   const client = new S3Client();
+  const bucket = process.env.BUCKET_NAME ?? '';
 
   describe('upload', () => {
     it.skip('オブジェクトが存在しない場合、uploadできる', async () => {
       const body = 'aaa';
-      const bucket = process.env.BUCKET_NAME ?? '';
       const fileName = 'text.txt';
 
       const res = await client.upload(body, bucket, fileName);
@@ -19,13 +19,55 @@ describe('S3Client', () => {
     });
 
     it.skip('すでにオブジェクトが存在していた場合、上書きする', async () => {
-      const bucket = process.env.BUCKET_NAME ?? '';
       const fileName = 'text2.txt';
-
       await client.upload('aaa', bucket, fileName);
       await client.upload('bbb', bucket, fileName);
 
       expect(1).toBe(1);
+    });
+  });
+
+  describe('listObjectsV2', () => {
+    it.skip('prefixを指定してない場合、全オブジェクトの情報を取得する', async () => {
+      const res = await client.listObjectsV2(bucket);
+
+      console.log(res);
+      expect(1).toBe(1);
+    });
+  });
+
+  describe('downloadCsvFile', () => {
+    it.skip('prefixを指定してない場合、全オブジェクトの情報を取得する', async () => {
+      const res = await client.downloadCsvFile(bucket, 'abc.csv');
+
+      console.log(res);
+      expect(1).toBe(1);
+    });
+  });
+
+  describe('deleteObject', () => {
+    it.skip('ファイルを削除する', async () => {
+      await client.deleteObject(bucket, 'abc.csv');
+
+      expect(1).toBe(1);
+    });
+  });
+
+  describe('copyObject', () => {
+    it.skip('オブジェクトをコピーできる', async () => {
+      await client.copyObject(bucket, 'text.txt', 'copy/test.csv');
+
+      expect(1).toBe(1);
+    });
+
+    it.skip('既にコピー先にファイルが存在している場合は上書きする', async () => {
+      await client.copyObject(bucket, 'text2.txt', 'copy/test.csv');
+      await client.copyObject(bucket, 'text.txt', 'copy/test.csv');
+      expect(1).toBe(1);
+    });
+
+    it.skip('コピー元のオブジェクトが存在しない場合はエラーになる', async () => {
+      await expect(client.copyObject(bucket, 'not-found.txt', 'copy/test.csv')).rejects.toThrow();
     });
   });
 });
--- packages/playground/src/aws/s3.ts
+++ packages/playground/src/aws/s3.ts
@@ -1,4 +1,5 @@
 import { S3 } from 'aws-sdk';
+import * as csvParse from 'fast-csv';
 
 /**
  * v2でS3を操作するクラス
@@ -33,5 +34,85 @@ export class S3Client {
     } catch (e) {
       console.log(e);
     }
+  }
+
+  /**
+   *
+   * @param bucketName
+   * @param prefix
+   */
+  async listObjectsV2(bucketName: string, prefix = '') {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    return await this.client.listObjectsV2({ Bucket: bucketName, Prefix: prefix }).promise();
+  }
+
+  /**
+   *
+   * @param fileName
+   * @param bucketName
+   */
+  async downloadCsvFile(bucketName: string, fileName: string): Promise<any> {
+    let parseStreamData;
+    const params = {
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      Bucket: bucketName,
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      Key: fileName,
+    };
+    try {
+      const csvReadStream = new S3().getObject(params).createReadStream();
+      const csvFileArray: Array<any> = [];
+      parseStreamData = new Promise((resolve, reject) => {
+        csvParse
+          .parseStream(csvReadStream, { headers: true })
+          .on('data', function (data) {
+            csvFileArray.push(data);
+          })
+          .on('end', function () {
+            resolve(csvFileArray);
+          })
+          .on('error', function () {
+            reject('csv parse process failed');
+          });
+      });
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+    return await parseStreamData;
+    // try {
+    //   return await parseStreamData;
+    // } catch {
+    //   return [];
+    // }
+  }
+
+  /**
+   *
+   * @param bucketName
+   * @param fileName
+   */
+  async deleteObject(bucketName: string, fileName: string) {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    await this.client.deleteObject({ Bucket: bucketName, Key: fileName }).promise();
+  }
+
+  /**
+   *
+   * @param bucketName
+   * @param source
+   * @param destination
+   */
+  async copyObject(bucketName: string, source: string, destination: string) {
+    return this.client
+      .copyObject({
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        Bucket: bucketName,
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        CopySource: `${bucketName}/${source}`,
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        Key: destination,
+      })
+      .promise();
   }
 }
