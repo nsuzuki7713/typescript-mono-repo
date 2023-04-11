9c32c062eb841f61ca246c30f3a6b18f04c1a458
nsuzuki7713 <nsuzuki7713@gmail.com>
2023-04-12 00:28:56 +0900

    追加

--- packages/playground/src/github-api/main.test.ts
+++ packages/playground/src/github-api/main.test.ts
@@ -5,4 +5,28 @@ dotenv.config();
 
 describe('Octokitを使ってGitHub APIを操作する', () => {
   const octokit = new Octokit({ auth: process.env.GITHUB_ACCESS_TOKEN });
+  const owner = process.env.GITHUB_OWNER ?? '';
+  const repo = process.env.GITHUB_REPO ?? '';
+
+  it('プルリクの一覧を取得する', async () => {
+    // https://docs.github.com/ja/rest/pulls/pulls?apiVersion=2022-11-28#list-pull-requests
+    const res = await octokit.rest.pulls.list({
+      owner,
+      repo,
+      state: 'all',
+    });
+
+    console.log(res.data);
+  });
+
+  it('プルリクの詳細を取得取得する', async () => {
+    // https://docs.github.com/ja/rest/pulls/pulls?apiVersion=2022-11-28#get-a-pull-request
+    const res = await octokit.rest.pulls.get({
+      owner,
+      repo,
+      pull_number: 178,
+    });
+
+    console.log(res.data);
+  });
 });
