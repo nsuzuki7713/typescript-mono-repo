936bde9868925d39e497b6cef4d0b6f7f0892f7d
nsuzuki7713 <nsuzuki7713@gmail.com>
2023-04-29 23:49:01 +0900

    追加

--- packages/playground/src/notion/main.test.ts
+++ packages/playground/src/notion/main.test.ts
@@ -42,7 +42,93 @@ describe('notion api', () => {
     console.log(response);
   });
 
-  it.only('ページにテキストを追加する', async () => {
+  it('プルリク作成テーブルにデータを追加', async () => {
+    const databaseId = process.env.NOTION_DATABASEID ?? '';
+    const response = await notion.pages.create({
+      parent: { database_id: databaseId },
+      properties: {
+        プルリク名: {
+          title: [
+            {
+              text: {
+                content: 'プルリク名',
+              },
+            },
+          ],
+        },
+        作成者: {
+          type: 'select',
+          select: {
+            name: '作成者',
+          },
+        },
+        削除行数: {
+          type: 'number',
+          number: 0,
+        },
+        作成日: {
+          type: 'date',
+          date: {
+            start: '2021-04-25 12:00',
+            time_zone: 'Asia/Tokyo',
+          },
+        },
+        修正ファイル: {
+          type: 'number',
+          number: 0,
+        },
+        approve時間: {
+          type: 'date',
+          date: {
+            start: '2021-04-25 12:00',
+            time_zone: 'Asia/Tokyo',
+          },
+        },
+        追加行数: {
+          type: 'number',
+          number: 0,
+        },
+        マージ時間: {
+          type: 'date',
+          date: {
+            start: '2021-04-25 12:00',
+            time_zone: 'Asia/Tokyo',
+          },
+        },
+        URL: {
+          type: 'url',
+          url: 'https://www.yahoo.co.jp/',
+        },
+      },
+    });
+
+    console.log(response);
+  });
+
+  it.only('サブアイテムを作成', async () => {
+    const databaseId = process.env.NOTION_DATABASEID ?? '';
+    const response = await notion.pages.create({
+      parent: { database_id: databaseId },
+      properties: {
+        レビュー者: {
+          type: 'select',
+          select: {
+            name: '作成者',
+          },
+        },
+        親アイテム: {
+          type: 'relation',
+          relation: [
+            {
+              id: '59d8f386-5a5d-407a-93fe-e3a2632dcfdb',
+            },
+          ],
+        },
+      },
+    });
+  });
+
+  it('ページにテキストを追加する', async () => {
     const pageId = process.env.NOTION_PAGEID ?? '';
     const response = await notion.blocks.children.append({
       block_id: pageId,
